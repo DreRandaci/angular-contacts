e21df0fafc18b60da77c3aafacc6902f62b8badc
Dre mf Randaci <drerandaci@gmail.com>
2017-11-15 11:16:48 -0600

    users can authenticate with google

--- javascripts/controllers/LoginCtrl.js
+++ javascripts/controllers/LoginCtrl.js
@@ -1,5 +1,14 @@
 'use strict';
 
-app.controller('LoginCtrl', function($scope){
-    $scope.controller = 'LoginCtrl';
+app.controller('LoginCtrl', function( $location, $rootScope, $scope, LoginService ){
+    $scope.authenticate = () => {
+        LoginService.authenticateGoogle().then(( results ) => {
+            $rootScope.uid = results.user.uid;
+            $scope.$apply(() => {
+                $location.url("/contacts/view");
+            }); 
+        }).catch(( err ) => {
+            console.log('error in authenticateGoogle:', err);
+        });
+    };
 });
