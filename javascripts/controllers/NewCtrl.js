bd8ed743623e2d0dffdaf96d8a9482f6542ef448
Dre mf Randaci <drerandaci@gmail.com>
2017-11-17 20:27:00 -0600

    contacts saving to firebase

--- javascripts/controllers/NewCtrl.js
+++ javascripts/controllers/NewCtrl.js
@@ -1,5 +1,12 @@
 'use strict';
 
-app.controller('NewCtrl', function($scope){
-    $scope.controller = 'NewCtrl';
+app.controller('NewCtrl', function( $location, $rootScope, $scope, ContactsService ){        
+    $scope.submitThisForm = (user) => {        
+        $scope.user.uid = $rootScope.uid;
+        ContactsService.postNewContact(user).then((results) => {
+            $location.path("/contacts/view");
+        }).catch((err) => {
+            console.log('error in postNewContact:', err);
+        });
+    };
 });
--- javascripts/services/ContactsService.js
+++ javascripts/services/ContactsService.js
@@ -2,7 +2,16 @@
 
 app.service("ContactsService", function( $http, $q, FIREBASE_CONFIG ){
     const getAllContacts = (userUid) => {
-        return $http.get(`${FIREBASE_CONFIG.databaseURL}/contacts.json?orderBy="uid"&equalTo="${userUid}"`);
+            return $http.get(`${FIREBASE_CONFIG.databaseURL}/contacts.json?orderBy="uid"&equalTo="${userUid}"`);
         };
-    return { getAllContacts };
+
+        const postNewContact = ( newContact ) => {
+            return $http.post(`${FIREBASE_CONFIG.databaseURL}/contacts.json`, JSON.stringify(newContact));
+        };
+
+        const deleteContact = (contactId) => {
+
+        };
+
+    return { getAllContacts, postNewContact, deleteContact };
 });
